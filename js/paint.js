cb698730f79dbd1a1b80ebe7aa7f9477473ce882
rodion <rodiongork@github.com>
2020-05-29 09:24:19 +0300

    Viewer for pics

--- js/paint.js
+++ js/paint.js
@@ -3,7 +3,7 @@ var paint = new PaintJS({paint: "#paint"});
 function onFinishPaint() {
 	var base64 = paint.canvas.toDataURL();
 	var xhr = new XMLHttpRequest();
-	xhr.open('POST', 'https://skripofon.ru/pths-quest.php/pics?k=denis.tkachev.2021b.1530416');
+	xhr.open('POST', 'https://skripofon.ru/pths-quest.php/pics?k=' + localStorage['userkey']);
 	xhr.send(base64);
 	alert('Окей... Зачтём. Переходи теперь обратно. Урок рисования закончен.');
 }
